f788651b83e7aea835b317b74bded80bc77cc44d
NidaFatima <2529nida17@gmail.com>
2024-11-07 22:20:37 +0530

    .

--- FrontEnd/src/components/Navbar.jsx
+++ FrontEnd/src/components/Navbar.jsx
@@ -1,33 +1,27 @@
-import React from 'react'
-import {assets} from '../assets/assets'
-import { NavLink } from 'react-router-dom'
+import React from 'react';
+import { assets } from '../assets/assets';
+import { NavLink } from 'react-router-dom';
+
 const Navbar = () => {
   return (
     <div>
-      <img src={assets.logo} alt="" />
+      <img src={assets.logo} alt="Logo" />
       <ul>
-        <NavLink>
-            <li>HOME</li>
-            <hr/>
-        </NavLink>
-        <NavLink>
-            <li>ALL DOCTORS</li>
-            <hr/>
-        </NavLink>
-        <NavLink>
-            <li>ABOUT</li>
-            <hr/>
-        </NavLink>
-        <NavLink>
-            <li>CONTACT</li>
-            <hr/>
-        </NavLink>
+        <li>
+          <NavLink to="/">HOME</NavLink>
+          <hr />
+        </li>
+        <li>
+          <NavLink to="/doctors">ALL DOCTORS</NavLink>
+          <hr />
+        </li>
+        <li>
+          <NavLink to="/about">ABOUT</NavLink>
+          <hr />
+        </li>
       </ul>
-      <div>
-        <button>Create account</button>
-      </div>
     </div>
-  )
+  );
 }
 
 export default Navbar;
