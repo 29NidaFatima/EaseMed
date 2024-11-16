39f92ee84962682df94a5d8ff8ba6d181dda70ad
NidaFatima <2529nida17@gmail.com>
2024-11-16 16:00:25 +0530

    .

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -12,10 +12,8 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 const App = () => {
   return (
-
     <div className='mx-4 sm:mx-[10%]'>
       <Navbar />
- 
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/doctors' element={<Doctors />} />
