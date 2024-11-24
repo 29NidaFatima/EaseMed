6d5387cdbf344db29ad6ecb17ace8f1c2f23250f
NidaFatima <2529nida17@gmail.com>
2024-11-24 13:19:52 +0530

    .

--- FrontEnd/src/pages/MyProfile.jsx
+++ FrontEnd/src/pages/MyProfile.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { assets } from '../assets/assets'
 
 const MyProfile = () => {
+  const[userData,setUserData]=useState({
+    name:"Edward Vincent",
+    image:assets.profile_pic,
+    email:'richardjameswap@gmail.com',
+    phone:'+1 123 456 7890',
+    address:{
+      line1:"57th cross,richmod",
+      line2:" Circle,Church Road,London"
+    },
+    gender:'Male',
+    dob:'2000-01-20'
+  })
+  const [isEdit,setIsEdit]=useState(false)
   return (
     <div>
-      
+      <img src={userData.image}  />
+
     </div>
   )
 }
