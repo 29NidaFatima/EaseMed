52e850343cd8d98015e2c0c20238d8fea8928f88
Nida fatima <2529nida17@gmail.com>
2024-11-26 05:06:00 +0530

    doctor controller and routes create api and integrate it with respective pages

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/mongodb.js'
+import doctorRouter from './routes/doctorRoute.js'
 import userRouter from './routes/userRoute.js'
 
 // app config
@@ -14,11 +15,12 @@ app.use(express.json())
 app.use(cors())
 
 // api end point
+app.use('/api/doctor', doctorRouter)
 app.use('/api/user', userRouter)
 
 
 app.get('/', (req, res) => {
-  res.send('Hiiii')
+    res.send('Hiiii')
 })
 
 app.listen(port, () => console.log('Server started', port)) 
