f250ae62bbc5b19034ceed7357f308c10df5ab85
Nida fatima <2529nida17@gmail.com>
2024-11-26 04:10:34 +0530

    create server and setup mvc with user route and controller

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,18 +1,22 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import Doctors from './pages/Doctors';
-import Login from './pages/Login';
-import About from './pages/About';
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import Doctors from './pages/Doctors'
+import Login from './pages/Login'
+import About from './pages/About'
 import Contact from './pages/Contact'
-import MyAppointment from './pages/MyAppointments';
-import MyProfile from './pages/MyProfile';
-import Appointment from './pages/Appointment';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import MyProfile from './pages/MyProfile'
+import MyAppointments from './pages/MyAppointments'
+import Appointment from './pages/Appointment'
+import Footer from './components/Footer'
+
 const App = () => {
   return (
     <div className='mx-4 sm:mx-[10%]'>
+      <ToastContainer />
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
@@ -21,15 +25,13 @@ const App = () => {
         <Route path='/login' element={<Login />} />
         <Route path='/about' element={<About />} />
         <Route path='/contact' element={<Contact />} />
-        <Route path='/my-appointments' element={<MyAppointment />} />
         <Route path='/my-profile' element={<MyProfile />} />
+        <Route path='/my-appointments' element={<MyAppointments />} />
         <Route path='/appointment/:docId' element={<Appointment />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </div>
-  );
-};
+  )
+}
 
-export default App;
-
-
+export default App
--- FrontEnd/src/pages/Login.jsx
+++ FrontEnd/src/pages/Login.jsx
@@ -1,76 +1,87 @@
-import React, { useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react'
+import { AppContext } from '../context/AppContext'
+import { toast } from 'react-toastify'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
 
 const Login = () => {
-  const [state, setState] = useState('Sign Up'); // Toggle between Sign Up and Login
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [name, setName] = useState('');
 
-  const onSubmitHandler = async (event) => {
-    event.preventDefault();
-    // Handle form submission here
-    console.log({ name, email, password, state });
-  };
+  const { backendUrl, token, setToken } = useContext(AppContext)
+  const navigate = useNavigate()
+  const [state, setState] = useState('Sign Up')
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault()
+
+    try {
+      console.log("state check", state)
+      if (state === 'Sign up') {
+        const { data } = await axios.post(backendUrl + '/api/user/register', { name, password, email })
+        if (data.success) {
+          localStorage.setItem('token', data.token)
+          setToken(data.token)
+        } else {
+          toast.error(data.message)
+        }
+      } else {
+        const { data } = await axios.post(backendUrl + '/api/user/login', { email, password })
+        if (data.success) {
+          localStorage.setItem('token', data.token)
+          setToken(data.token)
+        } else {
+          toast.error(data.message)
+        }
+      }
+
+    } catch (error) {
+      toast.error(error.message)
+    }
+  }
+
+  useEffect(() => {
+    if (token) {
+      navigate('/')
+    }
+  }, [token])
 
   return (
-    <form onSubmit={onSubmitHandler} className="min-h-[80vh] flex items-center">
-      <div className="flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-zinc-600 text-sm shadow-lg">
-        <p className="text-2xl font-semibold">{state === 'Sign Up' ? 'Create Account' : 'Login'}</p>
-        <p>Please {state === 'Sign Up' ? 'Sign Up' : 'log in'} to book an appointment</p>
-        {state === 'Sign Up' && (
-          <div className='w-full'>
-            <p>Full Name</p>
-            <input className='border border-zinc-300 rounded w-full p-2 mt-1'
-              type="text"
-              onChange={(e) => setName(e.target.value)}
-              value={name}
-              placeholder="Your full name"
-              required
+    <form onSubmit={onSubmitHandler} className='min-h-[80vh] flex items-center'>
+      <div className='flex flex-col gap-3 m-auto items-start p-8 min-w min-w-[340px] sm:min-w-96 border rounded-xl text-zinc-600 text-sm shadow-lg'>
+        <p className='text-2xl font-semibold'>{state === 'Sign Up' ? 'Create Account' : 'Login'}</p>
+        <p>Please {state === 'Sign Up' ? 'sign up' : 'log in'} to book appointment</p>
 
-            />
-          </div>
-        )}
+        {
+          state === 'Sign Up' && (
+            <div className='w-full'>
+              <p>Full Name</p>
+              <input className='border border-zinc-300 rounded w-full p-2 mt-1' type="text" onChange={(e) => setName(e.target.value)} value={name} required />
+            </div>
+          )
+        }
+
         <div className='w-full'>
           <p>Email</p>
-          <input
-            type="email"
-            onChange={(e) => setEmail(e.target.value)}
-            value={email}
-            placeholder="Your email"
-            required
-            className='border border-zinc-300 rounded w-full p-2 mt-1'
-          />
+          <input className='border border-zinc-300 rounded w-full p-2 mt-1' type="email" onChange={(e) => setEmail(e.target.value)} value={email} required />
         </div>
-        <div className='w-full' >
+
+        <div className='w-full'>
           <p>Password</p>
-          <input
-            type="password"
-            onChange={(e) => setPassword(e.target.value)}
-            value={password}
-            placeholder="Your password"
-            required
-            className='border border-zinc-300 rounded w-full p-2 mt-1'
-          />
+          <input className='border border-zinc-300 rounded w-full p-2 mt-1' type="password" onChange={(e) => setPassword(e.target.value)} value={password} required />
         </div>
-        <button
-          type="submit"
-          className="bg-blue-500 text-white px-4 py-2 rounded mt-4"
-        >
-          {state === 'Sign Up' ? 'Create Account' : 'Login'}
-        </button>
-        <p className="text-sm mt-2">
-          {state === 'Sign Up' ? 'Already have an account?' : "Don't have an account?"}{' '}
-          <button
-            type="button"
-            onClick={() => setState(state === 'Sign Up' ? 'Login' : 'Sign Up')}
-            className="text-blue-500 underline"
-          >
-            {state === 'Sign Up' ? 'Login' : 'Sign Up'}
-          </button>
-        </p>
+
+        <button type='submit' className='bg-primary text-white w-full py-2 rounded-md text-base'>{state === 'Sign Up' ? 'Create Account' : 'Login'}</button>
+
+        {
+          state === 'Sign Up'
+            ? <p>Already have an account? <span onClick={() => setState('Login')} className='text-primary underline cursor-pointer'>Login here</span></p>
+            : <p>Create an new accout? <span onClick={() => setState('Sign Up')} className='text-primary underline cursor-pointer'>click here</span></p>
+        }
       </div>
     </form>
-  );
-};
+  )
+}
 
-export default Login;
+export default Login
